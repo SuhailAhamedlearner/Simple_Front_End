402d24c14fb3a18342978b22c1fd493e9d6bd15f
Your Name <suhailshimla1@gmail.com>
2023-03-03 00:12:53 +0530

    First_commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const surpriseBtn = document.querySelector('.my-element');
+surpriseBtn.addEventListener('click', () => {
+  console.log('Button clicked!');
+  alert('You have been surprised!');
+});
